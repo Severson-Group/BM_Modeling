fb90129fbc49f342a65a417b9c4a35e7cc861cfb
Takahiro <114006024+noguchi-takahiro@users.noreply.github.com>
2025-05-19 00:57:13 -0500

    Add system ID feature based on MP model (#4)
    * Make folder
    
    * Add injection signal
    
    * Create bode plot
    
    * Modify code
    
    * Modify code
    
    * Move system ID folder in MP folder
    
    * Rename simlink model
    
    * Rename simlink model in m file

--- MP/SystemID/setup.m
+++ MP/SystemID/setup.m
@@ -21,18 +21,18 @@ elseif ENABLE_PWM == 1
     set_param('MP_BearinglessMotor/Voltage Source Inverter/Ideal Voltage Source Inverter', 'Commented', 'on'); 
 end
 
-Tend = 0.02; % Simulation stop time
+Tend = 10; % Simulation stop time
 
 V_DC = 160; % DC bus (V)
 
-torque_cmd = 0.2; % Torque reference (Nm)
+torque_cmd = 0; % Torque reference (Nm)
 torque_start = 0.002; % Torque start time (s)
 torque_end = 0.025; % Torque end time (s)
 
-fx_cmd = 9; % fx force reference (N)
+fx_cmd = 0; % fx force reference (N)
 fx_start = 0.002; % x-axis force start time (s)
 fx_end = 0.025; % x-axis force end time (s)
-fy_cmd = -4.5; % fy force reference (N)
+fy_cmd = 0; % fy force reference (N)
 fy_start = 0.001; % y-axis force start time (s)
 fy_end = 0.025; % y-axis force end time (s)
 
@@ -40,7 +40,12 @@ id_cmd = 0; % id current reference (A)
 id_start = 0; % d-axis current start time (s)
 id_end = 0.025; % d-axis current end time (s)
 
-speed_cmd = 7500; % Speed command (r/min)
+speed_cmd = 0; % Speed command (r/min)
+
+%% Signal injection
+f_init = 1; % Initial frequency of chirp signal (Hz)
+f_target = 1e3; % Traget frequency of chirp signal (Hz)
+vd_inj = 1; % Amplitude of d-axis voltage chirp (V)
 
 %% Plant parameters
 p = 4; % Number of torque pole-pair
@@ -93,7 +98,7 @@ Lt = L_alpha_beta(1,1);
 Ls = L_alpha_beta(3,3);
 
 %% Machine constants
-Ke = 0.003;  % back-emf constant (Vpk/(mech rad/s))
+Ke = 0;  % back-emf constant (Vpk/(mech rad/s))
 Kt = 0.02; % Torque constant (Nm/Apk)
 Kf = 1.8; % Force constant (N/Apk)
 
@@ -128,10 +133,7 @@ out = sim('BearinglessMotorSimulation.slx');
 runObj = Simulink.sdi.Run.getLatest;
 
 % List of variables to extract
-obj2ext = {'time','vd','vq','id_cmd','iq_cmd','id','iq','torque', ...
-                  'vx','vy','ix_cmd','iy_cmd','ix','iy','fx','fy', ...
-                  'phase_current', ...
-                  'EMF_1', 'EMF_2', 'EMF_3', 'EMF_4', 'EMF_5', 'EMF_6'};
+obj2ext = {'time','vd_inj','id'};
 
 % Get signal IDs and store signals into array
 for idx = 2:length(obj2ext)
@@ -142,6 +144,41 @@ end
 
 time = sig_obj.(obj2ext{2}).Values.Time;
 
+%% Select Data for FRF
+num = squeeze(sig_val.id);
+den = squeeze(sig_val.vd_inj);
+
+%% Generate FRF Data
+lines = 1e5;
+win = 'hann'; % try 'hann' or 'rectwin'
+
+[freq, mag, phase, coh] = generateFRF(num, den, Tsim, lines, win);
+
+%% Estimate RL
+% Find where frequency goes positive
+idx_f_pos = find(freq >= 0, 1);
+
+% Create data object of only positive freq response
+data = frd(mag(idx_f_pos:end).*exp(1j*deg2rad(phase(idx_f_pos:end))), 2*pi*freq(idx_f_pos:end));
+
+% Use the data object to estimate the transfer function
+num_poles = 1;
+sys = tfest(data, num_poles);
+
+% Print sys to console to see how well it worked
+sys
+
+[Z,gain] = zero(sys);
+P = pole(sys)
+
+Rp_estimated = -P/gain;
+Lp_estimated = 1/gain;
+
+fprintf('Estimated resistance: %0.8f, actual: %0.8f, error: %f pct\n', ...
+    Rp_estimated, R, 100 * (Rp_estimated - R)/R);
+fprintf('Estimated inductance: %0.8f, actual: %0.8f, error: %f pct\n', ...
+    Lp_estimated, Lt, 100 * (Lp_estimated - Lt)/Lt);
+
 %% Plot figure
 width = 2*5.43; 
 height = 1.2*3*4.38 / 3;
@@ -150,76 +187,23 @@ Inch_SS = get(0,'screensize');
 lw = 1;  % line width
 
 figure1 = figure;
-% Plot phase current
-subplot(3,2,1);
+% Plot voltage
+subplot(2,1,1);
 hold on;
-colors = lines(6); % Generate 6 types of colors
-for i = 1:6 % Columns 1-6 contain current data
-    plot(time, squeeze(sig_val.phase_current(:,i)), 'Color', colors(i,:), 'LineWidth', lw);
-end
 xlabel('Time [s]','Interpreter','latex');
-ylabel('Phase current (A)','Interpreter','latex');
-legend('Phase 1','Phase 2','Phase 3','Phase 4','Phase 5','Phase 6','Interpreter','latex','Location','east');
+plot(time, squeeze(sig_val.vd_inj), 'Color', 'k', 'LineWidth', lw);
+ylabel('Voltage (V)','Interpreter','latex');
+legend('$v_d$','Interpreter','latex','Location','east');
 xlim([0 Tend]);
 % ylim([-2.5 2.5]);
 
-% Plot back-emf
-subplot(3,2,2);
+% Plot current
+subplot(2,1,2);
 hold on;
-colors = lines(6); % Generate 6 types of colors
-plot(time, squeeze(sig_val.EMF_1), 'Color', colors(1,:), 'LineWidth', lw);
-plot(time, squeeze(sig_val.EMF_2), 'Color', colors(2,:), 'LineWidth', lw);
-plot(time, squeeze(sig_val.EMF_3), 'Color', colors(3,:), 'LineWidth', lw);
-plot(time, squeeze(sig_val.EMF_4), 'Color', colors(4,:), 'LineWidth', lw);
-plot(time, squeeze(sig_val.EMF_5), 'Color', colors(5,:), 'LineWidth', lw);
-plot(time, squeeze(sig_val.EMF_6), 'Color', colors(6,:), 'LineWidth', lw);
 xlabel('Time [s]','Interpreter','latex');
-ylabel('Back-EMF (V)','Interpreter','latex');
-legend('Phase 1','Phase 2','Phase 3','Phase 4','Phase 5','Phase 6','Interpreter','latex','Location','east');
-xlim([0 Tend]);
-% ylim([-2.5 2.5]);
-
-% Plot torque and force
-subplot(3,2,3);
-hold on;
-plot(time, squeeze(sig_val.id_cmd), 'Color', [231/255, 143/255, 106/255], 'LineWidth', lw);
-plot(time, squeeze(sig_val.iq_cmd), 'Color', [0/255, 108/255, 186/255], 'LineWidth', lw);
-plot(time, squeeze(sig_val.id), 'Color', [136/255, 62/255, 150/255], 'LineWidth', lw);
-plot(time, squeeze(sig_val.iq), 'Color', [237/255, 170/255, 33/255], 'LineWidth', lw);
-xlabel('Time [s]','Interpreter','latex');
-ylabel('Torque current (A)','Interpreter','latex');
-legend('$i_d^*$', '$i_q^*$', '$i_d$', '$i_q$','Interpreter','latex','Location','east');
-xlim([0 Tend]);
-% ylim([-2.5 2.5]);
-
-subplot(3,2,4);
-hold on;
-plot(time, squeeze(sig_val.ix_cmd), 'Color', [231/255, 143/255, 106/255], 'LineWidth', lw);
-plot(time, squeeze(sig_val.iy_cmd), 'Color', [0/255, 108/255, 186/255], 'LineWidth', lw);
-plot(time, squeeze(sig_val.ix), 'Color', [136/255, 62/255, 150/255], 'LineWidth', lw);
-plot(time, squeeze(sig_val.iy), 'Color', [237/255, 170/255, 33/255], 'LineWidth', lw);
-xlabel('Time [s]','Interpreter','latex');
-ylabel('Suspension current (A)','Interpreter','latex');
-legend('$i_x^*$', '$i_y^*$', '$i_x$', '$i_y$','Interpreter','latex','Location','east');
-xlim([0 Tend]);
-% ylim([-2.5 2.5]);
-
-subplot(3,2,5);
-hold on;
-plot(time, squeeze(sig_val.torque), 'Color', 'k', 'LineWidth', lw);
-xlabel('Time [s]','Interpreter','latex');
-ylabel('Torque (Nm)','Interpreter','latex');
-legend('Torque','Interpreter','latex','Location','east');
-xlim([0 Tend]);
-% ylim([-2.5 2.5]);
-
-subplot(3,2,6);
-hold on;
-plot(time, squeeze(sig_val.fx), 'Color', 'r', 'LineWidth', lw);
-plot(time, squeeze(sig_val.fy), 'Color', 'b', 'LineWidth', lw);
-xlabel('Time [s]','Interpreter','latex');
-ylabel('Force (N)','Interpreter','latex');
-legend('$f_x$','$f_y$','Interpreter','latex','Location','east');
+plot(time, squeeze(sig_val.id), 'Color', 'k', 'LineWidth', lw);
+ylabel('Current (A)','Interpreter','latex');
+legend('$i_d$','Interpreter','latex','Location','east');
 xlim([0 Tend]);
 % ylim([-2.5 2.5]);
 
@@ -228,3 +212,101 @@ set(findall(gcf, '-property', 'FontName'), 'FontName', 'Times New Roman');
 set(figure1,'Units','inches','Position',[(Inch_SS(3)-width)/2 (Inch_SS(4)-height)/2 width height]);
 print(figure1, '-dsvg','-noui','plot_results');
 print(figure1, '-dpng','-r300','plot_results');
+
+%% Plot Estimation
+
+% Show coherence plot?
+% 1: yes
+% 0: no
+SHOW_COHERENCE = 1;
+
+figure2 = figure;
+
+if SHOW_COHERENCE == 1
+    tiledlayout(3,1);
+    ax1 = nexttile;
+    ax2 = nexttile;
+    ax3 = nexttile;
+else
+    tiledlayout(2,1);
+    ax1 = nexttile;
+    ax2 = nexttile;
+end
+
+markersize = 8;
+
+plot(ax1, freq, mag, '.', 'markersize', markersize);
+plot(ax2, freq, phase, '.', 'markersize', markersize);
+
+hold (ax1, 'on'); 
+hold (ax2, 'on'); 
+
+% Set ideal transfer function here to overlay the curve fit
+Gp = tf([0 1], [Lt R]);
+[mag_ideal, phase_ideal] = bode(Gp, freq*2*pi);
+plot(ax1, freq, squeeze(mag_ideal), 'r');
+plot(ax2, freq, squeeze(phase_ideal), 'r');
+
+if SHOW_COHERENCE == 1
+    plot(ax3, freq, coh, '.', 'markersize', markersize);
+end
+
+ylabel(ax1, "Magnitude (-)");
+ylabel(ax2, "Phase (deg)");
+if SHOW_COHERENCE == 1
+    ylabel(ax3, "Coherence");
+end
+
+if SHOW_COHERENCE == 1
+    xlabel(ax3, "Frequency (Hz)");
+else
+    xlabel(ax2, "Frequency (Hz)");
+end
+
+f1 = f_init;
+f2 = f_target;
+xlim(ax1, [f1 f2]);
+xlim(ax2, [f1 f2]);
+
+grid(ax1, 'on');
+grid(ax2, 'on');
+
+set(ax1, 'xscale', 'log');
+set(ax2, 'xscale', 'log');
+
+% ylim(ax1, [0 6]);
+% ylim(ax2, [-120 10]);
+
+legend(ax1, {'FRF obtained from using tfestimate', 'Ideal FRF'}, 'Location', 'southwest');
+legend(ax2, {'FRF obtained from using tfestimate', 'Ideal FRF'}, 'Location', 'southwest');
+
+if SHOW_COHERENCE == 1
+    xlim(ax3, [f1 f2]);
+    ylim(ax3, [0 1.1]);
+    grid(ax3, 'on');
+    set(ax3, 'xscale', 'log');
+end
+
+set(findall(gcf, '-property', 'FontName'), 'FontName', 'Times New Roman');
+
+print(figure2, '-dsvg','-noui','bode_plot');
+print(figure2, '-dpng','-r300','bode_plot');
+
+%% generateFRF function
+function [freq, mag, phase, coh] = generateFRF(num, den, T, lines, win)
+
+	fs = 1/T;
+	overlap = lines/2;
+	averages = floor(length(num)/(lines-overlap));
+	windowType = window(win, lines);
+	
+	[FRF, freq] = tfestimate(den, num, windowType, overlap, lines, fs);
+	[coh, freq] = mscohere(den, num, windowType, overlap, lines, fs);
+	
+	FRF = fftshift(FRF);
+	coh = fftshift(coh);
+	freq = freq - max(freq)/2;
+	mag = abs(FRF);
+	phase = angle(FRF) * 180/pi;
+
+end
